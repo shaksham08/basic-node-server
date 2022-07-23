9de0279947d982e221d870ad2c3a37902c2a524e
Shaksham Sinha <shaksham@Shaksham-Sinhas-MacBook-Pro.local>
2022-07-23 19:00:59 +0530

    Added final code

--- server.js
+++ server.js
@@ -1,40 +1,61 @@
 const http = require("http");
 
-const PORT = 3005;
+const PORT = 3000;
 
 const server = http.createServer();
 
 const friends = [
   {
-    id: 1,
+    id: 0,
     name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
   },
   {
     id: 2,
     name: "Albert Einstein",
   },
-  {
-    id: 3,
-    name: "Sir Isaac Newton",
-  },
 ];
 
 server.on("request", (req, res) => {
-  const urlItems = req.url.split("/");
-  if (req.method === "POST" && urlItems[1] === "friend") {
-    const id = friends.length + 1;
+  const items = req.url.split("/");
+  // /friends/2 => ['', 'friends', '2']
+  // /friends/
+  if (req.method === "POST" && items[1] === "friends") {
     req.on("data", (data) => {
-      const friend = JSON.parse(data);
-      friend.id = id;
-      friends.push(friend);
-      console.log(friends);
+      const friend = data.toString();
+      console.log("Request:", friend);
+      friends.push(JSON.parse(friend));
     });
     req.pipe(res);
-  } else if (req.method === "GET" && urlItems[1] === "friends") {
-    res.end(JSON.stringify(friends));
-  } else res.end("HELLO");
+  } else if (req.method === "GET" && items[1] === "friends") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    if (items.length === 3) {
+      const friendIndex = Number(items[2]);
+      res.end(JSON.stringify(friends[friendIndex]));
+    } else {
+      res.end(JSON.stringify(friends));
+    }
+  } else if (req.method === "GET" && items[1] === "messages") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<body>");
+    res.write("<ul>");
+    res.write("<li>Hello Isaac!</li>");
+    res.write("<li>What are your thoughts on astronomy?</li>");
+    res.write("</ul>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
 });
 
-server.listen(3005, () => {
-  console.log("SERVER IS RUNNING AT LOCALHOST PORT " + PORT);
-});
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
+}); //127.0.0.1 => localhost
